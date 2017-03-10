61ff3b7039b8ef22cb00ade7ef7e6da5785a5198
AgenoDrei <s.mueller1995@gmail.com>
2017-03-10 09:13:57 +0100

    Fixed renderer problem (buggy webgl)

--- frontend/js/engine.js
+++ frontend/js/engine.js
@@ -8,7 +8,7 @@ class Engine {
     }
 
     init() {
-        this.pixiApp = new PIXI.Application(this.width, this.height, {backgroundColor : constants.background_color});
+        this.pixiApp = new PIXI.Application(this.width, this.height, {backgroundColor: constants.background_color}, true);
         this.parent.appendChild(this.pixiApp.view);
 
         var headline = new PIXI.Text(constants.headline_text, {font: "bold 32px serif", fill: "yellow"});
