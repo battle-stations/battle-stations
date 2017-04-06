b0a65ca447c2a89ab09afa6290450336f6a5417b
AgenoDrei <s.mueller1995@gmail.com>
2017-04-06 19:42:11 +0200

    Implemented tests for game over, game pausing, game representation and player indication

--- app/test/frontend.spec.js
+++ app/test/frontend.spec.js
@@ -1,26 +1,45 @@
 let assert =  chai.assert;
 let socket = null;
 let socket2 = null;
+let testToken = "";
 
 describe('Join game', function() {  
     it('should return token', function(done) {
     	socket = new DisplaySocket("Stuttgart", "Stadtmitte", 1);
     	socket.onToken = function(token) {
+    		testToken = token.token;
     		assert.equal(token.token, "Stuttgart_Stadtmitte_1");
     		done();
     	}
     });
 });
 
-describe('Get feedback about player input', function() {  
-    it('server should return ACK', function(done) {
+describe('Graphic representation', function() {  
+    it('Server should send latest game after join', function(done) {    	
     	socket2 = new DisplaySocket("Ulm", "Stadtmitte", 1);
-    	socket2.onToken = function(token) {
-    		let control = new ControlSocket(token.token);
+    	let run = false;
+    	socket2.onCurrentGame = function(game) {
+    		//console.log('Game: ', game);
+    		if(!run) {
+    			run = true;
+    			done();
+    		}
+    	};
+    });
+});
 
-    		control.sendLeftDown();
-    		control.onAck = done;
-    	}
+describe('Get feedback about player input', function() {  
+    it('server should return ACK', function(done) {    	
+    	let control = new ControlSocket(testToken);
+   		control.sendLeftDown();
+   		control.onAck = done;
+    });
+});
+
+describe('Player indication after join', function() {  
+    it('Player control should known team city', function() {    	
+    	let control = new ControlSocket(testToken);
+    	assert.equal("Stuttgart", testToken.split("_")[0]);
     });
 });
 
@@ -37,6 +56,20 @@ describe('Pause game', function() {
     	socket2.onIncomingTrain = function(msg) {
     		if(!paused) {
     			paused = true;
+    			done();
+    		}
+    	}
+    });
+}); 	 
+
+describe('Finish game', function() {  
+    it('should finish game after collision', function(done) {
+    	this.timeout(60000);
+    	let finished = false;
+    	socket.onOver = function() {
+    		//console.log('onOver');
+    		if(!finished) {
+    			finished = true;
     			done();
     		}
     	}
